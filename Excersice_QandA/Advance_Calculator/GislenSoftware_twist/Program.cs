622f7f7a8c6919a03cac6a178ac270d8b17f8e98
imrankhanv11 <146421206+imrankhanv11@users.noreply.github.com>
2025-06-19 18:06:48 +0530

    Add files via upload

---
+++ Excersice_QandA/Advance_Calculator/GislenSoftware_twist/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GislenSoftware_twist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    continue;
+                }
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Gislen Software");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Gislen");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Software");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
