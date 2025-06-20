e15547f847d0ff31dc1bb1f0f021587f923975b2
imrankhanv11 <146421206+imrankhanv11@users.noreply.github.com>
2025-06-20 09:09:11 +0530

    Add files via upload

---
+++ Excersice_QandA/Advance_Calculator/SafeNumberCollocter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeNumberCollocter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10]; 
+            int count = 0; 
+            string input;
+
+            
+            while (true)
+            {
+                Console.Write("Enter a number or type 'exit' to stop: ");
+                input = Console.ReadLine();
+
+                
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                // Try to convert the input to an integer
+                if (int.TryParse(input, out int number))
+                {
+                    // Resize array if it's full
+                    if (count == numbers.Length)
+                    {
+                        Array.Resize(ref numbers, numbers.Length * 2); // Double the size of the array
+                    }
+
+                    numbers[count] = number;
+                    count++;
+                }
+                else
+                {
+                    
+                    Console.WriteLine("Invalid input, please enter a valid integer.");
+                }
+            }
+
+            Console.WriteLine("\nValid integers collected:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
---
+++ Excersice_QandA/Advance_Calculator/SafeNumberCollocter/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SafeNumberCollocter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SafeNumberCollocter")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e6b3d57d-1a20-481d-a284-cd56dae2893e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
